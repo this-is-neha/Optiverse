1989507788590c3278f18576788e0ab0102f155c
this-is-neha <nehushah12@gmail.com>
2025-12-15 13:15:12 +0545

    Change

--- server/server.js
+++ server/server.js
@@ -6,7 +6,26 @@ const path = require("path");
 
 const app = express();
 const PORT = 3001;
+const cors = require("cors");
 
+const allowedOrigins = [
+  "https://optiverse.netlify.app", 
+  "http://localhost:5173", 
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+   
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/"); 
